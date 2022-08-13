7a376680cd4e4f9b3993267074313a9405935293
giridharan-natarajan <101104721+giridharan-natarajan@users.noreply.github.com>
2022-08-13 21:18:30 +0530

    Update and rename classforselenium.java to classforseleniumweek2.java

--- classforseleniumweek2.java
+++ classforseleniumweek2.java
@@ -6,7 +6,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class classforselenium {
+public class classforseleniumweek2 {
 	public static void main(String[] args) {
 		 WebDriverManager.chromedriver().setup();
 		 ChromeDriver driver=new ChromeDriver();
