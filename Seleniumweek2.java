3195324216fa2e31dc39e994155cc45f443f3b1e
giridharan-natarajan <101104721+giridharan-natarajan@users.noreply.github.com>
2022-08-13 21:18:57 +0530

    Update and rename Selenium2.java to Seleniumweek2.java

--- Seleniumweek2.java
+++ Seleniumweek2.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Selenium2 {
+public class Seleniumweek2 {
 	public static void main(String[] args) {
 		 WebDriverManager.chromedriver().setup();
 		 ChromeDriver driver=new ChromeDriver();
